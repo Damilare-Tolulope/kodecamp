6c34e24588864632ed1b2ffe0f908a10359ffa52
Damilare <54348429+Damilare-Tolulope@users.noreply.github.com>
2022-04-03 04:15:11 +0100

    app

--- src/App.js
+++ src/App.js
@@ -601,7 +601,10 @@ const App = () => {
     },
   ])
   return (
-    <div className="container">{allGiftCards.map(card => <Card key={card.id} card={card} />)}</div>
+    <div className="container">
+      <p style={{fontSize:'16px', fontWeight:'bold',marginBottom:'20px', textTransform:'uppercase'}}>Gift Cards and its currencies</p>
+      {allGiftCards.map(card => <Card key={card.id} card={card} />)}
+    </div>
   )
 }
 
